1a18b1d3dacc26a836146db0f99846591a9a851d
levannghia <levannghia.tdc2018@gmail.com>
2023-12-05 14:12:43 +0700

    Update

---
+++ src/components/propertyDetailsSections/PropertyHeaderSection.jsx
@@ -0,0 +1,69 @@
+import { StyleSheet, View, Share } from 'react-native'
+import React, { useState } from 'react'
+import { Text } from "@ui-kitten/components";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { default as theme } from '../../theme.json';
+import Row from '../Row';
+
+const PropertyHeaderSection = ({ property }) => {
+
+    const shareItem = async () => {
+        try {
+            await Share.share({
+                message: "Check out this sweet apartment I found on JPArtments.com.",
+            });
+        } catch (error) {
+            alert("Sorry, we're unable to share: " + error);
+        }
+    };
+
+    return (
+        <>
+            {property.name ? (
+                <Text category={"h5"} style={styles.defaultMarginTop}>
+                    {property.name}
+                </Text>
+            ) : null}
+            <Row style={[styles.containerRow, styles.defaultMarginTop]}>
+                <View>
+                    <Text category={"c1"}>{property.street}</Text>
+                    <Text category={"c1"}>{`${property.city}, ${getStateAbbreviation(
+                        property.state
+                    )} ${property.zip}`}</Text>
+                </View>
+                <Row style={styles.iconRow}>
+                    <MaterialIcons
+                        onPress={async () => {
+                            await shareItem();
+                        }}
+                        name="ios-share"
+                        size={30}
+                        color={theme["color-primary-500"]}
+                        style={styles.shareIcon}
+                    />
+                    <MaterialCommunityIcons
+                        onPress={handleHeartPress}
+                        name={property?.liked ? "heart" : "heart-outline"}
+                        size={30}
+                        color={theme["color-primary-500"]}
+                    />
+                </Row>
+            </Row>
+        </>
+    )
+}
+
+export default PropertyHeaderSection
+
+const styles = StyleSheet.create({
+    defaultMarginVertical: { marginVertical: 10 },
+    containerRow: {
+        justifyContent: "space-between",
+    },
+    iconRow: { paddingRight: 5 },
+    shareIcon: {
+        marginRight: 20,
+        marginTop: -4,
+    },
+    defaultMarginTop: { marginTop: 10 },
+});
--- src/screens/PropertyDetailsScreen.jsx
+++ src/screens/PropertyDetailsScreen.jsx
@@ -4,12 +4,14 @@ import { Screen } from '../components/screen'
 import ImageCarousel from '../components/ImageCarousel'
 import { Text } from '@ui-kitten/components'
 import { properties } from '../data/properties'
+import { default as theme } from '../../theme.json';
+import { LISTMARGIN } from '../../constant'
 // import { findIndex } from 'lodash'
 
-const PropertyDetailsScreen = ({route, style}) => {
-    const {propertyID} = route.params
-    const index = properties.findIndex(i => i.ID === propertyID);
-    const property = properties[index]
+const PropertyDetailsScreen = ({ route, style }) => {
+  const { propertyID } = route.params
+  const index = properties.findIndex(i => i.ID === propertyID);
+  const property = properties[index]
 
   return (
     <Screen>
@@ -17,9 +19,14 @@ const PropertyDetailsScreen = ({route, style}) => {
       <FlatList
         data={[property]}
         keyExtractor={(item) => item.ID}
-        renderItem={({item}) => {
-          <>{item?.images ? <ImageCarousel images={item.images}/> : null}</>
-        }}
+        renderItem={({ item }) => (
+          <>
+          {item?.images ? <ImageCarousel images={item.images} /> : null}
+          <View style={styles.contentContainer}>
+            
+          </View>
+          </>
+        )}
       />
     </Screen>
   )
@@ -27,4 +34,18 @@ const PropertyDetailsScreen = ({route, style}) => {
 
 export default PropertyDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    width: Dimensions.get("window").width,
+    height: 250,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+  },
+  contentContainer: {
+    marginHorizontal: LISTMARGIN,
+  },
+  divider: {
+    backgroundColor: theme["color-gray"],
+    marginTop: 10,
+  },
+})
